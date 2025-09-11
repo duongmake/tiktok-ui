e4aa971019061d0fb0051065fa9b0c6b1d4df28d
duongmake <duongmake3@gmail.com>
2025-09-11 10:56:56 +0700

    Tái cấu trúc và tối ưu code #1

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -20,7 +20,7 @@ import { UploadIcon } from '~/components/Icons';
 import Image from '~/components/Image';
 import Search from '../Search';
 import { Link } from 'react-router-dom';
-import routesConfig from '~/config/routes';
+import config from '~/config';
 
 const cx = classNames.bind(styles);
 
@@ -96,7 +96,7 @@ function Header() {
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
-        <Link to={routesConfig.home} className={cx('logo-link')}>
+        <Link to={config.routes.home} className={cx('logo-link')}>
           <img src={images.logo} alt="Tiktok" />
         </Link>
 
