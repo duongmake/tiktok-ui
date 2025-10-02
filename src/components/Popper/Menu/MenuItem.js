5fa999fd71d2909a1c4441db83147bfa6d594f10
duongmake <duongmake3@gmail.com>
2025-10-02 09:23:42 +0700

    Áp dụng PropTypes vào dự án

--- src/components/Popper/Menu/MenuItem.js
+++ src/components/Popper/Menu/MenuItem.js
@@ -1,6 +1,8 @@
+import PropTypes from 'prop-types';
 import Button from '~/components/Button';
 import classNames from 'classnames/bind';
 import styles from './Menu.module.scss';
+import { data } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
@@ -14,5 +16,10 @@ function MenuItem({ data, onClick }) {
     </Button>
   );
 }
+
+MenuItem.propTypes = {
+  data: PropTypes.object.isRequired,
+  onClick: PropTypes.func,
+};
 
 export default MenuItem;
