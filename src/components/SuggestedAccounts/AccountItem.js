2539f92c4bd41b3c7b8d8e57a04302bbc7990419
duongmake <duongmake3@gmail.com>
2025-10-06 15:41:41 +0700

    Xây dựng UI phần sidebar #3

---
+++ src/components/SuggestedAccounts/AccountItem.js
@@ -0,0 +1,47 @@
+import classNames from 'classnames/bind';
+import PropTypes from 'prop-types';
+import styles from './SuggestedAccounts.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react/headless';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountPreview from './AccountPreview';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+  const renderPreview = (props) => {
+    return (
+      <div tabIndex="-1" {...props}>
+        <PopperWrapper>
+          <AccountPreview />
+        </PopperWrapper>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <Tippy interactive delay={[800, 0]} offset={[-20, 0]} placement="bottom" render={renderPreview}>
+        <div className={cx('account-item')}>
+          <img
+            className={cx('avatar')}
+            src="https://files.fullstack.edu.vn/f8-tiktok/users/5321/657d63081d19a.jpg"
+            alt=""
+          />
+          <div className={cx('item-info')}>
+            <p className={cx('nickname')}>
+              <strong>Duongmake</strong>
+              <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+            </p>
+            <p className={cx('name')}>Phạm Tiến Dương</p>
+          </div>
+        </div>
+      </Tippy>
+    </div>
+  );
+}
+
+AccountItem.propTypes = {};
+
+export default AccountItem;
